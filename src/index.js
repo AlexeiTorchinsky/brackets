9241dec288f54aa68d33fb38b4f41ececd648b47
Alexei Torchinsky <alexey.tor@gmail.com>
2024-08-13 16:33:29 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
   const openBrackets = [];
-  const closeBrackets = [];
-  const bracketPair = {};
+  const closedBrackets = [];
+  const bracketPair = new Map();
 
-  bracketsConfig.forEach(pair => {
-    openBrackets.push(pair[0]);
-    closeBrackets.push(pair[1]);
-    bracketPair[pair[1]] = pair[0];
+  bracketsConfig.forEach((el) => {
+    openBrackets.push(el[0]);
+    closedBrackets.push(el[1]);
+    bracketPair.set(el[0], el[1]);
   });
 
   for (let i = 0; i < str.length; i++) {
-    const currentSymbol = str[i];
-    const topElement = stack[stack.length - 1];
-
-    if (openBrackets.includes(currentSymbol)) {
-      //проверка прямых скобок
-      if (currentSymbol === bracketPair[currentSymbol] && topElement === currentSymbol) {
+    //прямые скобки
+    if (bracketPair.get(str[i]) === str[i]) {
+      if (stack.length !== 0 && stack[stack.length - 1] === str[i]) {
         stack.pop();
       } else {
-        stack.push(currentSymbol);
+        stack.push(str[i]);
       }
-    } else {
-      if (stack.length === 0) {
-        return false;
-      }
+    }
 
+    //остальные скобки
+    if (openBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    }
 
-
-      if (bracketPair[currentSymbol] === topElement) {
+    if (closedBrackets.includes(str[i])) {
+      if (
+        str[i] === bracketPair.get(stack[stack.length - 1]) &&
+        stack.length !== 0
+      ) {
         stack.pop();
       } else {
         return false;
@@ -37,5 +38,4 @@ module.exports = function check(str, bracketsConfig) {
   }
 
   return stack.length === 0;
-}
-
+};
